5d732b6033a05c6ae86cd3decb234bdeeb68f1ad
DevanEunbeeLee <numerous.plutos@gmail.com>
2021-09-08 09:37:40 +0900

    calculator

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const calculator = {
     plus: function (a, b) {
         console.log(a + b);
     },
-    minus: 
+    minus: function (a, b) {
+        console.log(a - b);
+    },
+    divide: function (a, b) {
+        console.log(a / b);
+    },
+    multiple: function (a, b) {
+        console.log(a * b)
+    }
 };
 
 calculator.plus(13, 11);
+calculator.multiple(11, 393);
+
